ff8f51c7294fd4d54f393a673d2ac17c3e99e716
angelohavrelhuk <angelo.havrelhuk@univille.br>
2017-03-06 22:15:35 -0300

    commit

---
+++ aula_03/src/array.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class array {
+ int [] i = {7, 4, 5, 6};
+  Integer [] y;
+	
+public void sum(){
+ System.out.println(i.length);
+  JOptionPane.showMessageDialog(null, i[3]);
+   }
+    }
---
+++ aula_03/test/array_test.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class array_test {
+	
+@Test	
+public void sumTest(){
+array array01 = new array();	
+array01.sum();
+}
+}
